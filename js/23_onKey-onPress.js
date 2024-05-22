c478db7db8f34c31aaa4380021a4a6a1d8c60583
kjch777 <kjch911229@naver.com>
2024-05-22 14:32:25 +0900

    css js html 세분화

---
+++ js/23_onKey-onPress.js
@@ -0,0 +1,10 @@
+function 기능() {
+  const 기능 = document.getElementById("영문이름");
+  기능.value = 기능.value.toUpperCase();
+  //                    .toUpperCase(): 입력된 영문자를 자동으로 대문자로 변경
+  //                    .toLowerCase(): 입력된 영문자를 자동으로 소문자로 변경
+}
+
+function 엔터기능() {
+  alert("입력한 내용이 성공적으로 전달되었습니다.");
+}
